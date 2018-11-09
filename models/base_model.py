ed5354f760c99035063cc8d07c445468b02a558e
Essence Boayue <essenceboayue@Essences-MacBook-Pro.local>
2018-11-09 10:59:45 -0800

    Edit

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,10 @@ class BaseModel:
     """This class defines a Base Model
     Attributes:
     id (string): id number assigned when an instance is created
-    created_at (datetime): assigned with the current datetime when an instance is created
-    updated_at (datetime): assigned with the current datetime when an instance is created and is will be updated every time the object is changed
+    created_at (datetime): assigned with the current datetime when an instance
+    is created
+    updated_at (datetime): assigned with the current datetime when an instance
+    is created and it will be updated every time the object is changed
     """
 
     def __init__(self, *args, **kwargs):
@@ -25,9 +27,11 @@ class BaseModel:
             if "id" in kwargs.keys():
                 self.id = kwargs["id"]
             if "created_at" in kwargs.keys():
-                self.created_at = datetime.strptime(kwargs["created_at"], format)
+                self.created_at = datetime.strptime(kwargs["created_at"],
+                                                    format)
             if "updated_at" in kwargs.keys():
-                self.updated_at = datetime.strptime(kwargs["updated_at"], format)
+                self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                    format)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -38,7 +42,8 @@ class BaseModel:
         Returns:
             A string representation of the class name, id, and dict
         """
-        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__))
 
     def save(self):
         """Updates updated_at with the current datetime
@@ -46,7 +51,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance
+        """Returns a dictionary containing all keys/values of __dict__ of the
+        instance
         Returns:
             A dictionary with the specified keys included
         """
